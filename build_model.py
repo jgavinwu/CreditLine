004a9b86803a48c3b6f8da4f39155541d297d0da
jgavinwu <gavin.wu@bluestembrands.com>
2017-01-08 16:03:30 -0800

    Added required columns file and another ipython notebook

--- build_model.py
+++ build_model.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[1]:
+
 import os
 import re
 import json, codecs
@@ -12,95 +17,179 @@ from sklearn import (manifold, decomposition, ensemble, discriminant_analysis, r
 from sklearn import cross_validation, metrics
 from sklearn.grid_search import GridSearchCV
 
-y=datetime.strptime(x, '%Y-%m-%d-%H.%M.%S.000000')..strftime('%Y-%m-%d')
-y > parse('2016-11-01')
-os.chdir('/Users/gavin.wu/Python/Project/Orchard/Rodeo/orchard_plcc')
-# Read data
-train = pd.read_csv('orchard_plcc_train.csv', dtype=object)
-test = pd.read_csv('orchard_plcc_test.csv', dtype=object)
-# Read dictionary
-data = []
-with codecs.open('data_dict_orchard_plcc.txt','rU','utf-8') as f:
-    for line in f:
-       data.append(json.loads(line))
-# Read schema
-schema = pd.read_csv('final_schema_orchard_plcc.txt', header=None)
-schema = pd.Series.tolist(schema[0])
 
-# Preprocess
-indata = train
+# In[2]:
+
+def print_full(x):
+    pd.set_option('display.max_rows', len(x))
+    print(x)
+    pd.reset_option('display.max_rows')
+
+
+# In[9]:
+
+os.chdir('/project/')
+sam = pd.read_csv('./Data/cli_cma_joined_1pct.csv', dtype=object)
+# sam_cma = pd.read_csv('./Data/cli_cma_1pct.csv', dtype=object)
+# sam_all = pd.merge(sam, sam_cma, on=['CREDITCYCLEFACTMKEY'])
+sam.shape
+
+
+# In[10]:
+
+sam.columns
+
+
+# In[11]:
+
+sam_sum = sam.describe()
+print_full(sam_sum.transpose())
+
+
+# In[ ]:
+
+# all_col = sam.columns.str.upper()
+
+
+# In[12]:
+
+# import math
+# nan_col_ind = [math.isnan(val) if isinstance(val, float) else False for val in sam_sum.ix['top',]]
+nan_col_ind = (sam_sum.ix['count',] == 0)
+nan_col = sam.columns[nan_col_ind]
+nan_col
+# sam.drop(nan_col, axis=1, inplace=True)
+
+
+# In[13]:
+
+oneval_col_ind = (sam_sum.ix['unique',] == 1)
+oneval_col = sam.columns[oneval_col_ind]
+oneval_col
+
+
+# In[14]:
+
+# import re
+# [re.match('.*date.*', col, re.IGNORECASE) for col in all_col]
+# matches = all_col.str.findall('.*date.*', re.IGNORECASE)
+# matches.str.get(0).tolist()
+date_col_ind = sam.columns.str.contains('DATE')
+date_col = sam.columns[date_col_ind]
+date_col
+
+
+# In[15]:
+
+date_col2 = date_col.tolist()
+date_col2.remove('CYCLEDATE')
+date_col2
+
+
+# In[16]:
+
+to_drop = nan_col.tolist() + oneval_col.tolist() + date_col2
+sam2 = sam.drop(to_drop, axis=1)
+sam2.shape
+
+
+# In[23]:
+
+sam2['INCRTYPE'] = sam2['INCRTYPE'].fillna('Unknown')
+
+
+# In[31]:
+
+sam2['CURRENTDAYSPASTDUE'].unique()
+
+
+# In[25]:
+
+indata = sam2
 out_data = pd.DataFrame([])
-columns_in = indata.columns.tolist()
+columns_in = sam2.columns.tolist()
+columns_excl = ['CYCLEDATE', 'CUSTOMERSEGMENT', 'DWACCTID', 'CUSTOMERNUMBER', 'RANDOMNUM', 'CREDITCYCLEFACTMKEY']
 all_cat = []
 all_con = []
-for i in range(len(data)):
-    print i
-    col_name = data[i]['col_name']
-    # default = np.nan if data[i]['default_val']=='NA' else data[i]['default_val']
-    default = np.nan
-    if (col_name in columns_in):
-        col_out = indata[col_name]
-        if (data[i]['data_type']=='con'):
+for col_name in columns_in:
+    print col_name
+    col_out = indata[col_name]
+    if col_name not in columns_excl:
+        if (sam_sum.ix['unique',col_name] > 20):
             out_data[col_name] = pd.to_numeric(col_out)
-            all_con.append(i)
-        if (data[i]['data_type']=='cat'):
-            out_data[col_name] = pd.Series(pd.Categorical([v for v in col_out],categories=data[i]['snapshot']))
-            all_cat.append(i)
-    else: out_data[col_name] = default
+            all_con.append(col_name)
+        if (sam_sum.ix['unique',col_name] <= 20):
+            out_data[col_name] = pd.Series(pd.Categorical([v for v in col_out],categories=col_out.unique()))
+            all_cat.append(col_name)
+    else: out_data[col_name] = col_out
 
-out_data[out_data.columns[all_cat]]=out_data[out_data.columns[all_cat]].apply(lambda x:x.fillna(x.value_counts().index[0]))
-out_data[out_data.columns[all_con]]=out_data[out_data.columns[all_con]].apply(lambda x:x.fillna(x.median()))
-# out_data_dum = pd.get_dummies(out_data, prefix_sep='__', sparse=True)
-out_data_dum = pd.get_dummies(out_data, prefix_sep='__')
-out_data_dum = out_data_dum.to_sparse(fill_value=0)
-out_data_dum_sc = scale(out_data_dum)
 
-plcc_train = pd.DataFrame(out_data_dum_sc)[schema]
-plcc_train_target = pd.to_numeric(train['PLCC_OFR_CONV'])
-plcc_test = pd.DataFrame(out_data_dum_sc)[schema] # reusing the code block above
-plcc_test_target = pd.to_numeric(test['PLCC_OFR_CONV'])
+# In[32]:
 
-# Non-linear dimension reduction
-# X = out_data_dum_sca.copy()
-# n_neighbors = 20
-# X_iso = manifold.Isomap(n_neighbors, n_components=2).fit_transform(X)
-# plt.scatter(X_iso[:,0], X_iso[:,1])
+out_data['CUSTOMERSEGMENT'] = pd.Series(pd.Categorical([v for v in out_data['CUSTOMERSEGMENT']],categories=out_data['CUSTOMERSEGMENT'].unique()))
 
-# Fit a single model
-xgb_setup = XGBClassifier(
-     learning_rate=1,
-     n_estimators=100,
-     max_depth=20,
-     min_child_weight=5,
-     gamma=5,
-     subsample=1,
-     colsample_bytree=0.4,
-     objective='binary:logistic',
-     nthread=4,
-#     scale_pos_weight=1,
-     missing=np.nan,
-     seed=206)
-xgb_model = xgb_setup.fit(plcc_train, plcc_train_target, early_stopping_rounds=10, eval_metric="auc", eval_set=[(plcc_test, plcc_test_target)])
-# dtrain = xgb.DMatrix(cc, cct, missing=np.nan)
-# dtest = xgb.DMatrix(cc_test, cc_testt, missing=np.nan)
-pred_test = xgb_model.predict_proba(plcc_test)
-pred_test1 = [x[1] for x in pred_test]
-metrics.roc_auc_score(plcc_test_target, pred_test1)
 
-# Grid search
-grid = {
-    'learning_rate': [0.4, 0.8]
-}
+# In[49]:
 
-gs = GridSearchCV(
-    estimator=xgb_setup,
-    param_grid=grid,
-    scoring='roc_auc',
-    n_jobs=2,
-    iid=False,
-    cv=5)
+out = out_data.sort_values(['CUSTOMERNUMBER', 'CYCLEDATE']).groupby(['CUSTOMERNUMBER']).last().reset_index()
 
-gs.fit(cc, cct)
-pred_test = gs.predict_proba(cc_test)
-pred_test1 = [x[1] for x in pred_test]
-metrics.roc_auc_score(cc_testt, pred_test1)
+
+# In[100]:
+
+con_ft_raw = out_data[all_con + ['CUSTOMERNUMBER']]
+min_ft = con_ft_raw.groupby('CUSTOMERNUMBER', group_keys=False).apply(lambda x:x.min())
+max_ft = con_ft_raw.groupby('CUSTOMERNUMBER').apply(lambda x:x.max())
+con_ft = pd.merge(min_ft, max_ft, on='CUSTOMERNUMBER', suffixes=('_min', '_max'))
+
+
+# In[104]:
+
+last_ft = out.drop(['DWACCTID', 'CYCLEDATE', 'RANDOMNUM', 'CREDITCYCLEFACTMKEY', 'CYCLEDUE'], axis=1)
+last_ft['CUSTOMERNUMBER'] = last_ft['CUSTOMERNUMBER'].map(float)
+all_ft = pd.merge(con_ft, last_ft, on='CUSTOMERNUMBER', suffixes=('', '_last'))
+
+
+# In[108]:
+
+target_raw = out_data[['CUSTOMERNUMBER', 'CYCLEDUE']]
+target = target_raw.groupby('CUSTOMERNUMBER').apply(lambda x:x.max())
+
+
+# In[111]:
+
+dmat = pd.get_dummies(all_ft, prefix_sep='__')
+
+
+# In[115]:
+
+dmat.drop(['CUSTOMERNUMBER'], axis=1, inplace=True)
+target.drop(['CUSTOMERNUMBER'], axis=1, inplace=True)
+
+
+# In[116]:
+
+xgb_model = xgb.XGBRegressor().fit(dmat, target)
+
+
+# In[123]:
+
+wt = xgb_model.booster().get_score(importance_type='weight')
+
+
+# In[128]:
+
+wt2 = pd.DataFrame(wt.items(), columns=['Feature', 'Weight'])
+print_full(wt2.sort_values(['Weight'], ascending=False))
+
+
+# In[131]:
+
+dmat.shape
+
+
+# In[8]:
+
+one_cust_id = sam_all['DWACCTID_x'].sample(1)
+one_cust = sam_all.ix[sam_all['DWACCTID_x'].isin(one_cust_id),:]
+print_full(one_cust)
+
